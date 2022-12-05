6db080ffccf35047ae024cd8b98c0772524f0eb7
Jackson Edwards <35614363+jacksone1@users.noreply.github.com>
2022-12-05 15:17:02 +0000

    Commented out parts of Complasa_runner

--- src/complasa_runner.cpp
+++ src/complasa_runner.cpp
@@ -13,35 +13,35 @@ int main () {
   
     Complex a(5.0,6.0),b;
 
-    std::cout << "Enter b: ";
-    std::cin >> b;
+    //std::cout << "Enter b: ";
+    //std::cin >> b;
 
-    std::cout << "a = " << a << "\n";
-    std::cout << "b = " << b << "\n";
+    //std::cout << "a = " << a << "\n";
+    //std::cout << "b = " << b << "\n";
 
-    std::cout << "a + b = " << a + b << "\n";
-    std::cout << "a * b = " << a * b << "\n";
-    std::cout << "a / b = " << a / b << "\n";
-    std::cout << "|a| = "   << abs(a) << "\n";
-    std::cout << "complex conjugate of a = " << conj(a) << "\n";
-    std::cout << "norm of a = " << norm(a) << "\n";
-    std::cout << "abs of a = " << abs(a) << "\n";
-    std::cout << "exp(a) = " << exp(a) << "\n";
+    //std::cout << "a + b = " << a + b << "\n";
+    //std::cout << "a * b = " << a * b << "\n";
+    //std::cout << "a / b = " << a / b << "\n";
+    //std::cout << "|a| = "   << abs(a) << "\n";
+    //std::cout << "complex conjugate of a = " << conj(a) << "\n";
+    //std::cout << "norm of a = " << norm(a) << "\n";
+    //std::cout << "abs of a = " << abs(a) << "\n";
+    //std::cout << "exp(a) = " << exp(a) << "\n";
 
     Complex j = Complex(0.0,1.0);
-    Complex z1 = 1+2*j;
-    Complex z2 = 2+3*j;
+    //Complex z1 = 1+2*j;
+    //Complex z2 = 2+3*j;
 
-    z1 += z2;
+    //z1 += z2;
 
-    cout << "Complex number example: " << z1 << "\n";
-    cout << "The conjugate is: " << conj(z1) << "\n";
-    cout << "The magnitude is: " << abs(z1) << "\n";
-    cout << "The phase in Radians is: " << arg(z1) << "\n";
-    cout << "The norm is: " << norm(z1) << "\n";
-    cout << "The sqrt is: " << sqrt(z1) << "\n";
-    cout << "The natural log is: " << log(z1) << "\n";
-    cout << "The sin is: " << sin(z1) << "\n";
+    //cout << "Complex number example: " << z1 << "\n";
+    //cout << "The conjugate is: " << conj(z1) << "\n";
+    //cout << "The magnitude is: " << abs(z1) << "\n";
+    //cout << "The phase in Radians is: " << arg(z1) << "\n";
+    //cout << "The norm is: " << norm(z1) << "\n";
+    //cout << "The sqrt is: " << sqrt(z1) << "\n";
+    //cout << "The natural log is: " << log(z1) << "\n";
+    //cout << "The sin is: " << sin(z1) << "\n";
 
     /* other function testing */
     /*
